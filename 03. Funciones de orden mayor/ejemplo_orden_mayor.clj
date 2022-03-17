cc6a0cdb81eabc3fba86f9bf914af0e2c74de188
a01752785 <a01752785@itesm.mx>
2022-03-17 12:53:55 -0600

    Implements my-map

--- 03. Funciones de orden mayor/ejemplo_orden_mayor.clj
+++ 03. Funciones de orden mayor/ejemplo_orden_mayor.clj
@@ -11,3 +11,12 @@
 (def f3 (compuesta f1 f2))
 
 (def f4 (compuesta f2 f1))
+
+; Propia version de map
+
+(defn my-map
+  [fun & args]
+  (if (some empty? args)
+    ()
+    (cons (apply fun (map first args))
+          (apply my-map fun (map rest args)))))
