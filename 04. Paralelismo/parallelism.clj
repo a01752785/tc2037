4b449f9c795d0f3de6f09aca255b899ab0d78339
a01752785 <a01752785@itesm.mx>
2022-04-05 15:42:32 -0500

    Parallelism first problem

--- 04. Paralelismo/parallelism.clj
+++ 04. Paralelismo/parallelism.clj
@@ -14,4 +14,44 @@
       (recur (inc i)
              (*' i r))))))
 
-(fact-sec 5)
+(defn fact-range
+  [[start end]]
+  (loop [i start
+         r 1]
+    (if (= i end)
+      r
+      (recur (inc i)
+             (*' i r)))))
+
+(defn compute-ranges
+  [n p]
+  (partition 2
+             1
+             (concat (range 1 n (quot n p))
+          [(inc n)])))
+
+(compute-ranges 1000 5)
+
+(defn fact-par
+  [n]
+  (->> (.availableProcessors (Runtime/getRuntime))
+       (compute-ranges n)
+       (pmap fact-range)
+       (reduce *')
+       bits))
+
+(fact-par 1000)
+
+(time (fact-sec 5))
+(time (fact-sec 1000))
+(time (fact-sec 10000))
+(time (fact-sec 100000))
+
+(time (fact-par 5))
+
+(time (fact-sec 100000))
+(time (fact-par 100000))
+
+;;; T1 = 4496.2358
+;;; T8 = 485.8042
+;;; Sp = 9.25
