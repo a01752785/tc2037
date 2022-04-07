53ed244d6ee5cd33ca73dfcbe54cec6bc6d70e74
a01752785 <a01752785@itesm.mx>
2022-04-07 12:50:21 -0500

    Implements parallelism problem 4

--- 04. Paralelismo/parallelism.clj
+++ 04. Paralelismo/parallelism.clj
@@ -55,3 +55,60 @@
 ;;; T1 = 4496.2358
 ;;; T8 = 485.8042
 ;;; Sp = 9.25
+
+; Problema 4
+
+(defn create-random-data
+  [n]
+  (repeatedly n #(rand-int 1000)))
+
+(apply <= (create-random-data 1000))
+
+(defn insertion-sort
+  [s]
+  (loop [s s
+         r ()]
+    (if (empty? s)
+      r
+      (let [x (first s)
+            [before after] (split-with #(< % x) r)]
+        (recur (rest s)
+               (concat before [x] after))))))
+
+(insertion-sort [5 2 9 8 1 7 4 3])
+
+(defn merge-algorithm
+  [a b]
+  (loop [a a
+         b b
+         r ()]
+    (cond
+      (empty? a) (concat (reverse r) b)
+      (empty? b) (concat (reverse r) a)
+      (< (first a) (first b)) (recur (rest a) b (cons (first a) r))
+      :else (recur a (rest b) (cons (first b) r)))))
+
+(merge-algorithm [1 2 5 7] [1 3 4 5 6 7 8 9])
+
+(defn hybrid-sort-seq
+  [s]
+  (if (< (count s) 100)
+    (insertion-sort s)
+    (let [[left right] (split-at (quot (count s) 2) s)]
+      (merge-algorithm
+        (hybrid-sort-seq left)
+        (hybrid-sort-seq right)))))
+
+(defn hybrid-sort-par
+  [s]
+  (if (< (count s) 100)
+    (insertion-sort s)
+    (let [splitted (split-at (quot (count s) 2) s)]
+      (apply merge-algorithm
+        (pmap hybrid-sort-par splitted)))))
+
+(def n 100000)
+(def random-data (create-random-data n))
+(apply <= random-data)
+(apply <= (time (hybrid-sort-seq random-data)))
+(apply <= (time (hybrid-sort-par random-data)))
