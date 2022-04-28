194fa06b61bb7bea609d7e3a990eaf8ff832faca
a01752785 <a01752785@itesm.mx>
2022-04-28 08:49:21 -0500

    Problem Set 6 Docstrings

--- 06. Automata/automata.clj
+++ 06. Automata/automata.clj
@@ -1,3 +1,11 @@
+;----------------------------------------------------------
+; Problem Set #6: Automata
+; Date: April 28, 2022.
+; Authors:
+;          A01752789 Luis Humberto Romero Perez
+;          A01752785 David Damian Galan
+;----------------------------------------------------------
+
 (ns automata
   (:require [clojure.test :refer [deftest is run-tests]]))
 (defrecord DFA [initial-state
@@ -5,6 +13,9 @@
                 transitions])
 ;Accepts function
 (defn accepts?
+  "Function that takes a DFA using the record format and an input string
+  that will be applied in the automaton. It traverses through the DFA
+  and returns true if the ending state is an accepting state, false otherwise."
   [dfa input]
   (let [initial-state (.initial_state dfa)
         accept-states (.accept_states dfa)
@@ -16,7 +27,11 @@
         (recur (rest input)
                ((transitions current) (first input)))))))
 ;Problem 1
-(def dfa-1 (->DFA :q0
+
+(def dfa-1
+  "Automata with alphabet {a,b} that accepts a string
+  containing the 'ab' substring"
+  (->DFA :q0
                   #{:q2}
                   {:q0 {\a :q1
                         \b :q0}
@@ -34,7 +49,10 @@
   (is (not (accepts? dfa-1 "baa")))
   (is (not (accepts? dfa-1 "bbba"))))
 ;Problem 2
-(def dfa-2 (->DFA :q0
+(def dfa-2
+  "Automata with alphabet {0, 1} that accepts a string starting with 0
+  and ending with 1, an rejects any other string"
+  (->DFA :q0
                  #{:q2}
                   {:q0 {\0 :q1
                        \1 :q3}
@@ -54,7 +72,11 @@
   (is (not (accepts? dfa-2 "1001011")))
   (is (not (accepts? dfa-2 "1001010"))))
 ;Problem 3
-(def dfa-3 (->DFA :q0
+
+(def dfa-3
+  "Automata with alphabet {x, y} that accepts string containing
+  the substring 'yyy' and rejects any other string"
+  (->DFA :q0
                   #{:q3}
                   {:q0 {\x :q0
                         \y :q1}
@@ -74,7 +96,10 @@
   (is (not (accepts? dfa-3 "yxxyxxy")))
   (is (not (accepts? dfa-3 "xyxyyxyyx"))))
 ;Problem 4
-(def dfa-4 (->DFA :q0
+(def dfa-4
+  "Automata with alphabet {i, j, k} that accept strings with even length
+  and rejects string with odd length"
+  (->DFA :q0
                   #{:q0}
                   {:q0 {\i :q1
                         \j :q1
@@ -93,7 +118,10 @@
   (is (not (accepts? dfa-4
                      "kjikjikjikjikjikjikjikjikji"))))
 ;problem 5
-(def dfa-5 (->DFA :q0
+(def dfa-5
+  "Automata with alphabet {s, t} that accepts a string not containing
+  two or more consecutive characters and rejects otherwise"
+  (->DFA :q0
                   #{:q0 :q1 :q3}
                   {:q0 {\s :q1
                         \t :q3}
@@ -116,7 +144,11 @@
                      "tttttttttttttttttttttttttt"))))
 
 ;Problem 6
-(def dfa-6 (->DFA :q0
+(def dfa-6
+  "Automaton with alphabet {#, $} that accepts a string where each
+  character # is followed by exactly one or three $ symbols, and rejects
+  otherwise"
+  (->DFA :q0
                  #{:q0 :q2 :q4}
                  {:q0 {\# :q1
                        \$ :q0}
@@ -140,7 +172,12 @@
   (is (not (accepts? dfa-6 "$$$$$#$###$$$$#")))
   (is (not (accepts? dfa-6 "#$#$#$#$#$$$#$$$#$$$#"))))
 ;Problem 7
-(def dfa-7 (->DFA :q0
+
+(def dfa-7
+  "Automaton with alphabet {@, %} that accepts a string containing
+  exactly two times the character @, not necessary consecutive, and rejects
+  anything else"
+  (->DFA :q0
                   #{:q2}
                   {:q0 {\@ :q1
                         \% :q0}
